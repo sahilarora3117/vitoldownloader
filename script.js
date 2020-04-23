09fe7d4920bbde0454314be5e90c7f8ca2fdc3fd
sahilarora3117 <iamsahil@protonmail.com>
2020-04-23 17:15:18 +0530

    changed to downloading API

--- script.js
+++ script.js
@@ -1,12 +1,43 @@
 
+
 function downloadVideo () {
     browser.tabs.executeScript({file: "/vitol.js"})
-    .then(console.log("downloading"))
+    .then( downloader)
     .catch(reportExecuteScriptError);
+    
 }
 document.getElementById("download").addEventListener("click", downloadVideo); 
+document.getElementById("download").click()
+
+var showShowError = true;
 
 function reportExecuteScriptError() {
-    document.querySelector("#error-content").classList.remove("hidden");
-    document.querySelector("#download").classList.add("hidden");
-  }
+    if (showShowError) {
+        document.querySelector("#error-content").classList.remove("hidden");
+        document.querySelector("#download").classList.add("hidden");
+    
+    }
+    }
+
+  function downloader() {
+    browser.runtime.onMessage.addListener(handleMessage);
+    async function handleMessage(request) {
+            function onStartedDownload(id) {
+                showShowError  = false;
+              }
+              
+              function onFailed(error) {
+                console.log(`Download failed: ${error}`);
+              }
+                        
+              var downloading =  browser.downloads.download({
+                url : request.video_link,
+                filename : request.video_name
+              });
+              
+             await downloading.then(onStartedDownload, onFailed);
+             showShowError  = false;
+
+          }
+        
+}
--- vitol.js
+++ vitol.js
@@ -3,8 +3,21 @@ var ROOT = "http://vitol.ac.in";
 var video_asset = get_video_asset ();
 var video_name = extract_video_name (video_asset)
 var video_link =  ROOT + video_asset;
-download_video (video_link, video_name);
+// download_video (video_link, video_name);
+var sending = browser.runtime.sendMessage({
+    video_link: video_link,
+    video_name: video_name
+  });
+  sending.then(handleResponse, handleError);  
 
+function handleResponse(message) {
+    console.log(`Message from the background script:  ${message.video_link}`);
+}
+  
+function handleError(error) {
+    console.log(`Error: ${error}`);
+}
+  
 function get_video_asset () {
     return document.getElementsByClassName("video-player")[0].children[0].children[0].children[0].getAttribute ("src");
 }
@@ -13,12 +26,3 @@ function extract_video_name (video_asset) {
     return video_asset.slice(video_asset.lastIndexOf("@")+1, video_asset.lastIndexOf("?"));
 }
 
-function download_video (video_link, video_name) {
-    var downloader = document.createElement("a");
-    downloader.download = video_name;
-    downloader.href = video_link;
-    document.body.appendChild(downloader);
-    downloader.click();
-    document.body.removeChild(downloader);
-    delete downloader;
-}
